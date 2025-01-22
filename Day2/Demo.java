25f7878ab6b4a5bab32c7a32199a13c788219317
UTKARSH SINGH <103371307+UtkarshSingh-codes@users.noreply.github.com>
2025-01-22 21:00:43 +0530

    Add files via upload

---
+++ Day2/Demo.java
@@ -0,0 +1,36 @@
+
+
+
+class Demo
+
+{
+         public static void main(String arg[])
+{
+
+         //creat an object of MethodDemo
+          MethodDemo md1=new MethodDemo();
+
+          md1.a=10;
+          md1.b=20;
+          
+          md1.add();
+         
+          md1.sub();
+          md1.mul();
+	md1.div();
+
+          MethodDemo md2 = new MethodDemo();
+          md2.a=100;
+          md2.b=50;
+
+          md2.add();
+          md2.sub();
+	md1.mul();
+	md1.div();
+
+
+
+}
+
+
+}
