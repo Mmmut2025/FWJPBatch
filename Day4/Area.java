37681bc12cf5d18b71d38b29f5350cd2dbcfc753
UTKARSH SINGH <103371307+UtkarshSingh-codes@users.noreply.github.com>
2025-01-28 11:28:36 +0530

    Add files via upload

---
+++ Day4/Area.java
@@ -0,0 +1,20 @@
+class Area
+
+{
+
+	public static void main(String args[]) 
+	{
+
+		double length = 45;
+
+		double breath = 15;
+
+		double area = length*breath;
+
+	System.out.println("Area of Rectangle : " +area);
+
+
+
+}
+
+}
